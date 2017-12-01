1384a8a93dd43ab03aaecc8fdf88bc3803ed39b6
Arnaud Fanthomme <afanthomme@gmail.com>
2017-12-01 23:54:30 +0100

    Minor changes

--- src/network.py
+++ src/network.py
@@ -12,34 +12,30 @@ class GraphBandit:
         self.sources = GraphView(self.network, vfilt=lambda v: v.ext_flow != 0).vertices()
         self.state = None
 
-    def get_extflow(self):
-        # Get the Poisson-distributed external flow
-        # If any node goes overstack, negative reward
-        # If reach a terminal state, positive reward
+    def evolution_step(self):
+        # Use the current value of the flow to diffuse the stack
         stack = self.network.vertex_properties["stack"]
         capacity = self.network.vertex_properties["capacity"]
+        flow = self.network.vertex_properties["flow"].a
         ext_flow = self.network.vertex_properties["ext_flow"].a
+
+        diffusion_kernel = flow # Infinitesimal diffusion step with the current flow
+        stack.a = diffusion_kernel.dot(stack.a)
+
+        # After evolution, get the external in and out flows
         arrivals = np.zeros_like(ext_flow)
         for i in range(self.network.num_vertices()):
             if ext_flow[i] > 0:
                 arrivals[i] = np.random.poisson(ext_flow[i])
+            elif ext_flow[i] < 0:
+                arrivals[i] = ext_flow[i]
 
-        stack.a = np.min(stack.a + arrivals, capacity.a)
-        reward = -np.sum(stack.a == capacity.a)
-
-
-    def diffuse_state(self):
-        # Use the chosen value of the flow to evolve the state
-        stack = self.network.vertex_properties["stack"]
-        capacity = self.network.vertex_properties["capacity"]
-        flow = self.network.vertex_properties["flow"]
-
-        stack.a = np.min(flow.a.dot(stack.a), capacity.a)
-        reward = -np.sum(stack.a == capacity.a)
-        pass
+        stack.a = np.max(np.min(stack.a + arrivals, capacity.a), 0)
+        reward = -np.sum(stack.a == capacity.a)    # Could be changed to penalize more large mistakes.
+        return reward
 
     def flow_policy(self):
-        # Given the current estimate of the sources and the state of the network,
-        # choose the flow to set
+        # Given the current estimate of the sources and the state of the network, choose the flow to set for the next
+        # evolution step.
         pass
 
